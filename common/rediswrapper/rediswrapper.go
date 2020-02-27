f37d16b33c784ce77438be07b3f3bad32992cb73
narasimha <narasimha@192-168-1-107.tpgi.com.au>
2020-02-27 22:36:26 +1100

    changed to use redis wrapper

--- common/rediswrapper/rediswrapper.go
+++ common/rediswrapper/rediswrapper.go
@@ -4,6 +4,8 @@ import "github.com/go-redis/redis"
 
 type Redis interface {
 	RPush(string, string) (int64, error)
+	LRange(string, int64, int64) ([]string, error)
+	LRem(string, int64, interface{}) (int64, error)
 }
 
 type redisClientWrapper struct {
@@ -14,6 +16,14 @@ func NewRedisClientWrapper(client *redis.Client) Redis {
 	return &redisClientWrapper{
 		client: client,
 	}
+}
+
+func (r *redisClientWrapper) LRem(key string, count int64, value interface{}) (int64, error) {
+	return r.client.LRem(key, count, value).Result()
+}
+
+func (r *redisClientWrapper) LRange(key string, start int64, stop int64) ([]string, error) {
+	return r.client.LRange(key, start, stop).Result()
 }
 
 func (r *redisClientWrapper) RPush(key string, value string) (int64, error) {
--- recoverer/recoverer.go
+++ recoverer/recoverer.go
@@ -4,6 +4,7 @@ import (
 	"github.com/go-redis/redis"
 	"github.com/nmjmdr/jobber/common/constants"
 	"github.com/nmjmdr/jobber/common/models"
+	"github.com/nmjmdr/jobber/common/rediswrapper"
 	"github.com/nmjmdr/jobber/dlock"
 	"github.com/pkg/errors"
 )
@@ -13,18 +14,20 @@ type Recoverer interface {
 }
 
 type recoverer struct {
-	client *redis.Client
+	client rediswrapper.Redis
+	locker dlock.Lock
 }
 
-func NewRecoverer(client *redis.Client) Recoverer {
+func NewRecoverer(client rediswrapper.Redis, locker dlock.Lock) Recoverer {
 	return &recoverer{
 		client: client,
+		locker: locker,
 	}
 }
 
 func (r *recoverer) Recover() error {
 	// read head of processing queue
-	results, err := r.client.LRange(constants.InProcessQueue, 0, 0).Result()
+	results, err := r.client.LRange(constants.InProcessQueue, 0, 0)
 	if err != nil && err != redis.Nil {
 		return errors.Wrap(err, "Error while trying to recover jobs in Recoverer")
 	}
@@ -40,8 +43,7 @@ func (r *recoverer) Recover() error {
 	}
 
 	// is there a active lock on the job?
-	lock := dlock.NewLock(r.client.Pipeline())
-	isLocked, err := lock.IsLocked(job.Id)
+	isLocked, err := r.locker.IsLocked(job.Id)
 	if err != nil {
 		return errors.Wrap(err, "Could not check for lock in recover")
 	}
@@ -53,11 +55,11 @@ func (r *recoverer) Recover() error {
 	// Pushing to worker queue and removing from in process queue should not cause any issues
 	// There is a chance that we end up with two entries for the job in in_process_queue
 	// but then we remove one of them below, the latest one is locked by the worker and we wont delete it
-	_, err = r.client.RPush(constants.WorkerQueueName(job.Type), results[0]).Result()
+	_, err = r.client.RPush(constants.WorkerQueueName(job.Type), results[0])
 	if err != nil {
 		return errors.Wrap(err, "Unable to push job %s to worker queue")
 	}
-	_, err = r.client.LRem(constants.InProcessQueue, 1, results[0]).Result()
+	_, err = r.client.LRem(constants.InProcessQueue, 1, results[0])
 	if err != nil {
 		return errors.Wrap(err, "Unable to delete job %s from perocessing queue")
 	}
